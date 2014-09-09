e7c4082052e091715a866c9c3cc566d05dbd9057
Alessandro Oliveira da Silva <sandrobahia8@(none)>
2014-09-09 14:35:35 -0300

    Adicionando a questao 01 da primeira lista

---
+++ Questao_01.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+// O codigo recebe dois inteiros e os imprime trocando os valores das variaveis 
+int main()
+{   
+    fflush (stdin);
+    int a = 0, b = 0, aux = 0;
+    printf("Insira dois numeros inteiros: 'a' e 'b' \n\n");
+    fflush (stdin);
+    scanf("%d \n\n",&a);
+    fflush (stdin);
+    scanf("%d \n\n",&b);
+    fflush (stdin);
+    aux = a;
+    a = b;
+    b = aux;
+    printf("\nAgora o valor de 'a' eh: %d \n\nE o valor de 'b' eh: %d \n\n",a, b);
+    
+    system ("pause");
+    return 0;   
+}
+*/
